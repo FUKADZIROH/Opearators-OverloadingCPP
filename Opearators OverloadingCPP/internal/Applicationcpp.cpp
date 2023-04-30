50e153046525e8208c59384c06f227ea7731e071
Ігор Джурбій <ihordzhurbiy@gmail.com>
2023-04-30 15:42:24 +0300

    Fill operators C++

--- Opearators OverloadingCPP/internal/Applicationcpp.cpp
+++ Opearators OverloadingCPP/internal/Applicationcpp.cpp
@@ -2,25 +2,14 @@
 
 int main() 
 {
-	//Point p1(12, 14);
-	//cout << "p1 = "; p1.print();
-	//Point p2(0, 7);
-	//cout << "p2 = "; p2.print();
-	//Point p3 = p1 / p2;
-	//cout << "p3 = "; p3.print();
-
 	try
 	{
-		Point p1(12, 14);
-		Point temp = ++p1;
-		cout << "temp = "; temp.print();
-		cout << "p1 = "; p1.print();
-
-
-		//Point p2(0, 0);
-		//cout << "p2 = "; ++p2.print();
-		//Point p3 = p1 / p2;
-		//cout << "p3 = "; p3.print();
+		Point point1;
+		Point point2;
+		cin >> point1;
+		cin >> point2;
+		cout << point1;
+		cout << point2;
 	}
 	catch (const char* msg) //ловить throw
 	{
