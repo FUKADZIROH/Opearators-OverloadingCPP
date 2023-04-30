4ec8a3311ee87260c7e019e9992ef9f0ecf1517e
Ігор Джурбій <ihordzhurbiy@gmail.com>
2023-04-30 15:06:55 +0300

    Update main

--- Opearators OverloadingCPP/internal/Applicationcpp.cpp
+++ Opearators OverloadingCPP/internal/Applicationcpp.cpp
@@ -1,8 +1,31 @@
-#include "../domain/model/point.h"
+#include "../domain/provider/operators_overload.h"
 
 int main() 
 {
-	Point x(1, 2);
-	x.print();
+	//Point p1(12, 14);
+	//cout << "p1 = "; p1.print();
+	//Point p2(0, 7);
+	//cout << "p2 = "; p2.print();
+	//Point p3 = p1 / p2;
+	//cout << "p3 = "; p3.print();
+
+	try
+	{
+		Point p1(12, 14);
+		Point temp = ++p1;
+		cout << "temp = "; temp.print();
+		cout << "p1 = "; p1.print();
+
+
+		//Point p2(0, 0);
+		//cout << "p2 = "; ++p2.print();
+		//Point p3 = p1 / p2;
+		//cout << "p3 = "; p3.print();
+	}
+	catch (const char* msg) //ловить throw
+	{
+		cout << msg << endl;
+	}
+
 	return 0;
 }
